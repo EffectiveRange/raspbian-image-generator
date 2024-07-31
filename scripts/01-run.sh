aba38a5a88e3e48cc1f7a54e2370e8e4394cc57b
AttilaGombosER <attila.gombos@effective-range.com>
2024-07-31 16:20:34 +0200

    Revert workaround as issue is fixed now (https://github.com/piwheels/packages/issues/462) (#8)

--- scripts/01-run.sh
+++ scripts/01-run.sh
@@ -6,19 +6,11 @@
 
 cp files/*.json "${ROOTFS_DIR}/tmp/"
 
-RELEASE_INFO=$(cat "${ROOTFS_DIR}/etc/os-release")
-RELEASE_VERSION=$(echo "${RELEASE_INFO}" | grep VERSION_ID | cut -d '"' -f 2)
-
 on_chroot << EOF
 
 python -m venv /tmp/venv
 
 source /tmp/venv/bin/activate
-
-if [ "${RELEASE_VERSION}" = "11" ]; then
-  # Workaround for cryptography 43.0.0 forcing the use of OpenSSL 3 which is not available in Debian 11
-  pip install --force-reinstall -v cryptography==42.0.8
-fi
 
 pip install debian-package-installer@git+https://github.com/EffectiveRange/debian-package-installer.git@latest
 
