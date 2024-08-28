6034f8c4603eae5e66b2047cffd67700ed9a796c
GitHub Action <action@github.com>
2024-08-28 13:34:26 +0000

    Update version to 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='raspbian-image-generator',
-    version='1.2.2',
+    version='1.3.0',
     description='Raspberry Pi image generator using the official pi-gen repository',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
